cd680ce2f9c0f3b6170275f4240217c78afe31ba
unknown <infoman.rafael@gmail.com>
2024-10-01 09:31:46 -0400

    Configuracion de extensiones Tailwind para VS Code

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,78 +1,69 @@
-import { Link, Head } from '@inertiajs/react';
+import { Link, Head } from "@inertiajs/react";
 
 const people = [
     {
-      name: 'Calvin Hawkins',
-      email: 'calvin.hawkins@example.com',
-      image:
-        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        name: "Calvin Hawkins",
+        email: "calvin.hawkins@example.com",
+        image: "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
     {
-      name: 'Kristen Ramos',
-      email: 'kristen.ramos@example.com',
-      image:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        name: "Kristen Ramos",
+        email: "kristen.ramos@example.com",
+        image: "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
     {
-      name: 'Ted Fox',
-      email: 'ted.fox@example.com',
-      image:
-        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        name: "Ted Fox",
+        email: "ted.fox@example.com",
+        image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
-  ]
+];
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     const handleImageError = () => {
-        document.getElementById('screenshot-container')?.classList.add('!hidden');
-        document.getElementById('docs-card')?.classList.add('!row-span-1');
-        document.getElementById('docs-card-content')?.classList.add('!flex-row');
-        document.getElementById('background')?.classList.add('!hidden');
+        document
+            .getElementById("screenshot-container")
+            ?.classList.add("!hidden");
+        document.getElementById("docs-card")?.classList.add("!row-span-1");
+        document
+            .getElementById("docs-card-content")
+            ?.classList.add("!flex-row");
+        document.getElementById("background")?.classList.add("!hidden");
     };
 
     return (
         <>
             <Head title="Welcome" />
-            <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-                <div className="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-                    <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                        <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                            <nav className="-mx-3 flex flex-1 justify-end"> 
-                                <>
-                                    <Link
-                                        href={route('login')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Log in
-                                    </Link>
-                                    <Link
-                                        href={route('register')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Register
-                                    </Link>
-                                </>
-                            </nav>
-                        </header>
-                        <div>
-                            hola
-                            {/* On: "bg-indigo-600", Off: "bg-gray-200" */}
-                            <span aria-checked="false" class="bg-gray-200 relative inline-block flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline" role="checkbox" tabindex="0">
-                            {/* On: "translate-x-5", Off: "translate-x-0" */}
-                            <span aria-hidden="true" class="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200"></span>
-                            </span>
-                            <ul className="divide-y divide-gray-200">
-                                {people.map((person) => (
-                                    <li key={person.email} className="py-4 flex">
-                                    <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-                                    <div className="ml-3">
-                                        <p className="text-sm font-medium text-gray-900">{person.name}</p>
-                                        <p className="text-sm text-gray-500">{person.email}</p>
-                                    </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+            <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                <div class="shrink-0">
+                    <img
+                        class="size-12"
+                        src="/img/logo.svg"
+                        alt="ChitChat Logo"
+                    />
+                </div>
+                <div>
+                    <div class="text-xl font-medium text-black">ChitChat</div>
+                    <p class="text-slate-500">You have a new message!</p>
+                </div>
+            </div>
+            <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+                <img
+                    class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+                    src="/img/erin-lindford.jpg"
+                    alt="Woman's Face"
+                />
+                <div class="text-center space-y-2 sm:text-left">
+                    <div class="space-y-0.5">
+                        <p class="text-lg text-black font-semibold">
+                            Erin Lindford
+                        </p>
+                        <p class="text-slate-500 font-medium">
+                            Product Engineer
+                        </p>
                     </div>
+                    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                        Message
+                    </button>
                 </div>
             </div>
         </>
